19e0747d16bac6c313d028ac3087750a320e91df
CKS <ckseng86@gmail.com>
2016-09-20 01:51:35 +1000

    added create board function to javascript

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,14 +1,23 @@
-console.log("JS file is connected to HTML! Woo!")
+// var cardOne = "queen";
+// var cardTwo = "king";
+// var cardThree = "king";
+// var cardFour = "queen";
 
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "king";
-var cardFour = "queen";
+// if (choiceOne === choiceTwo) {
+// 	alert("You found a match!");
+// } else {
+// 	alert("Sorry, try again.");
+// }
 
-var choiceOne = cardTwo;
-var choiceTwo = cardFour;
-if (choiceOne === choiceTwo) {
-	alert("You found a match!");
-} else {
-	alert("Sorry, try again.");
+var gameBoard = document.getElementById('game-board');
+
+var createBoard = function() {
+	var noOfCards = 4;
+	for (var i = 0; i < noOfCards; i++) {
+		var newCard = document.createElement('div');
+		newCard.className = 'card';
+		gameBoard.appendChild(newCard);
+	}
 }
+
+createBoard();
